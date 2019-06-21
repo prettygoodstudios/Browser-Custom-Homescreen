f63f34dbd53749d47fd8be6e93ef292ee7d5f6d4
Miguel Rust <hava12soft@gmail.com>
2019-06-21 17:13:37 -0600

    Stylized Newsfeed.

--- src/components/newsFeed.js
+++ src/components/newsFeed.js
@@ -20,28 +20,39 @@ class NewsFeed extends Component {
                         return(
                             <div className="news-wrapper__feed-wrapper">
                                 <h3>{title}</h3>
-                                <ul>
-                                    {
-                                        data.articles && data.articles.splice(0, 3).map((a, i) => {
-                                            const {author, url, content, urlToImage} = a;
-                                            const articleTitle = a.title;
-                                            return(
-                                                <li key={i} onClick={() => window.location = url}>
-                                                    <div className="article-img-wrapper">
-                                                        <img src={urlToImage} />
-                                                    </div>
-                                                    <div>
-                                                        <h3 href={url}>{articleTitle}</h3>
-                                                        <p>{author} - {content && content.split("[+")[0]}</p>
-                                                    </div>
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                </ul>
-                                <button>
-                                    View More
-                                </button>
+                                {   (data.articles && data.articles.length > 0) ?
+                                    <ul>
+                                        {
+                                            data.articles && data.articles.splice(0, 3).map((a, i) => {
+                                                const {author, url, content, urlToImage} = a;
+                                                const articleTitle = a.title;
+                                                return(
+                                                    <li key={i} onClick={() => window.location = url}>
+                                                        <div className="article-img-wrapper">
+                                                            <img src={urlToImage} />
+                                                        </div>
+                                                        <div className="article-body">
+                                                            <h3 href={url}>{articleTitle}</h3>
+                                                            <p>{author} - {content && content.split("[+")[0]}</p>
+                                                        </div>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                        <li className="view-more">
+                                            <div>
+                                                <button>
+                                                    View More Articles
+                                                </button>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    :
+                                    <div>
+                                        <h3>Sorry, there are no articles available at this time.</h3>
+                                    </div>
+                                }
+                          
                             </div>
                         )
                     })}
