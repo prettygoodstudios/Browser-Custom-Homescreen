d47c76c22e21f895b667b2ad5c9ef19891ede917
Miguel Rust <hava12soft@gmail.com>
2019-06-21 21:24:48 -0600

    implemented quick links.

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,14 @@ import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 
 import reducers from "../reducers";
+import githubIcon from "../images/github.png";
+import playIcon from "../images/music.png";
+import youtubeIcon from "../images/youtube.png";
+import driveIcon from "../images/drive.png";
 
 import NewsFeed from "./newsFeed";
 import Weather from "./weather";
+
 
 const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
@@ -15,7 +20,23 @@ export default class App extends Component {
         return(
             <Provider store={store}>
                 <div>
-                    <Weather />
+                    <div className="header-wrapper">
+                        <Weather />
+                        <div className="link-wrapper">
+                            <a href="https://www.github.com/prettygoodstudios">
+                                <img src={githubIcon} />
+                            </a>
+                            <a href="https://www.youtube.com">
+                                <img src={youtubeIcon} />
+                            </a>
+                            <a href="https://play.google.com/music/listen?u=0">
+                                <img src={playIcon} />
+                            </a>
+                            <a href="https://drive.google.com/drive/u/0/my-drive">
+                                <img src={driveIcon} />
+                            </a>
+                        </div>
+                    </div>
                     <NewsFeed />
                 </div>
             </Provider>
